e5a4dff6fcd371796d9933798114742ab306d55c
PenTest-duck <chris.yoo@safetyculture.io>
2024-02-26 01:06:42 +1100

    frontend stretch task 1

--- frontend/unilectives-frontend/src/components/title.tsx
+++ frontend/unilectives-frontend/src/components/title.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 
 const Title = () => {
+  const [titleColour, setTitleColour] = useState("text-unilectives-blue");
+
+  const toggleTitleColour = () => {
+    if (titleColour === "text-unilectives-blue") {
+      setTitleColour("text-red-300");
+    } else {
+      setTitleColour("text-unilectives-blue");
+    }
+  }
+
   return (
-    <div className="title">
+    <div 
+      className="title"
+      onClick={toggleTitleColour}
+    >
       <p>DevSoc presents</p>
-      <h1 className="font-bold text-unilectives-blue text-7xl">unilectives</h1>
+      <h1 className={`font-bold text-7xl ${titleColour}`}>unilectives</h1>
       <p className="font-bold mt-3">Your one-stop shop for UNSW course and elective reviews.</p>
     </div>
   );
